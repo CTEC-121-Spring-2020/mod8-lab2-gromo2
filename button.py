4db7e3bb4f1751b005d55259866a5b05e1add013
Garrett <g.romo2@students.clark.edu>
2020-06-10 12:38:15 -0700

    Lab

---
+++ button.py
@@ -0,0 +1,10 @@
+# button.py
+class Button:
+    # constructor
+    def __init__(self):
+        print("button created")
+# unit testing
+def buttonTest():
+    b = Button()
+if __name__ == '__main__':
+    buttonTest()
